3b689baeb89e6a7cc926368df6b2f118bef48c2f
Clément Lusson <clementlusson@gmail.com>
2018-04-05 15:59:55 +0200

    changing zoom and marker position

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,12 +18,13 @@ export default {
   },
   data() {
     return {
-      zoom: 13,
-      center: L.latLng(47.41322, -1.219482),
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 15,
+      center: L.latLng(47.218371, -1.553621),
+      url:
+        "https://api.mapbox.com/styles/v1/clusson/cjf9cr2wz4c8e2qqfbyjqm5sl/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2x1c3NvbiIsImEiOiJjamY5Y2o2NmwxaHl1MnhtbXl0ejdsMG10In0.t5OT9RxZcRdtmE_3a19tag",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(47.41322, -1.219482)
+      marker: L.latLng(47.206078, -1.538607)
     };
   }
 };
